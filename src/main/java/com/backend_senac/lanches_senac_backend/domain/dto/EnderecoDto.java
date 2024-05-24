879cc54e70e9df4dd2accf686aab5f509543aeb9
Taís Marcolino <tais.marcolino@gmail.com>
2024-05-24 20:51:21 -0300

    Ajustes

--- src/main/java/com/backend_senac/lanches_senac_backend/domain/dto/EnderecoDto.java
+++ src/main/java/com/backend_senac/lanches_senac_backend/domain/dto/EnderecoDto.java
@@ -1,4 +1,11 @@
 package com.backend_senac.lanches_senac_backend.domain.dto;
 
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
 public class EnderecoDto {
 }
